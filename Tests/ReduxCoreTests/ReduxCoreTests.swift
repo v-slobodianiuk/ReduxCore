1d6b5698c9200551289a54e144d0849afd0288f0
Vadym Slobidianiuk <dev.slobodianiuk@gmail.com>
2021-12-15 18:56:26 +0200

    Remove Tests

--- Tests/ReduxCoreTests/ReduxCoreTests.swift
+++ Tests/ReduxCoreTests/ReduxCoreTests.swift
@@ -6,6 +6,5 @@ final class ReduxCoreTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ReduxCore().text, "Hello, World!")
     }
 }
